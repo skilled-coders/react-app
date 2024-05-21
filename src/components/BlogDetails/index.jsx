86b472906cec85df5f00af2f467f3e41d2d634df
yash_patil_neovasolutions <yash_patil@neovasolutions.in>
2024-05-21 17:04:00 +0530

    add empty divs blog detail component

--- src/components/BlogDetails/index.jsx
+++ src/components/BlogDetails/index.jsx
@@ -1,6 +1,12 @@
 function BlogDetails() {
     return (
-        <></>
+        <>
+            <div className="imageBanner">
+            </div>
+            <div className="title"></div>
+            <div className="description"></div>
+            <div className="content"></div>
+        </>
     );
 };
 
