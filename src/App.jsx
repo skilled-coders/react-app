a6ae0b3df239ebd56337d3927087bad34f81969b
yash_patil_neovasolutions <yash_patil@neovasolutions.in>
2024-05-22 17:24:02 +0530

    learn useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import "./App.css";
 import Blog from "./components/Blog"
+import { useState } from "react";
 
 function App() {
-  const blogs = [
+  const [blogs, setBlogs] = useState([
     {
       id: 1,
       title: "Securing Node.js in Production: Expert Practices for Every Developer",
@@ -15,25 +16,43 @@ function App() {
       description: "Perspiciatis sunt ducimus facere est voluptates tempora. Totam eius dignissimos dicta maiores.",
       imgUrl: "https://miro.medium.com/v2/da:true/resize:fill:140:140/0*oY_ucPmMlywwHFow"
     }
-  ];
+  ]);
 
-  const blogTitles = blogs.map((blog) => {
-    return { blogId: blog.id, title: blog.title }
-  });
+  const generateNewBlog = () => {
+    return {
+      id: blogs.length + 1,
+      title: `Blog number ${blogs.length + 1}`,
+      description: `Blog description ${blogs.length + 1}`,
+      imgUrl: "https://miro.medium.com/v2/da:true/resize:fill:140:140/0*3Xdd_WEaRxryzfLC"
+    }
+  }
 
-  console.log(blogTitles);
+  function handleClickMe(display) {
+    setDisplay(!display);
+  }
+
+  const [display, setDisplay] = useState(true);
+
+  function handleAddBlog() {
+    const newBlog = generateNewBlog();
+    setBlogs([...blogs, newBlog]);
+  }
 
   return (
     <>
       <h2>My Blogs</h2>
-
+      <button onClick={() => handleAddBlog()}>Add Blog</button>
       {blogs.map((blog) => (
         <Blog
+          key={blog.id}
           title={blog.title}
           description={blog.description}
           imgUrl={blog.imgUrl}
+          show={blog.id === 2 ? (display ? true : false) : true}
         />
       ))}
+
+      <button onClick={() => handleClickMe(display)}>{display ? "Hide" : "Show"} second blog</button>
     </>
   )
 }
--- src/components/Blog/index.jsx
+++ src/components/Blog/index.jsx
@@ -1,17 +1,34 @@
 import "./styles/Blog.css";
+import { useState } from "react";
 
-function Blog({ title, description, imgUrl }) {
+function Blog({ title, description, imgUrl, show }) {
+    const [color, setColor] = useState("red");
+
+    const handleColorChange = () => {
+        if (color === "red") {
+            setColor("black");
+        } else {
+            setColor("red");
+        }
+    }
+
     return (
         <>
-            <div className="blogContainer">
-                <div className="details">
-                    <h3>{title}</h3>
-                    <p>{description}</p>
-                </div>
-                <div className="imageWrapper">
-                    <img src={imgUrl} alt="" />
-                </div>
-            </div>
+            {
+                show ? (
+                    <div className="blogContainer">
+                        <div className="details">
+                            <h3 style={{ color: color }} onClick={() => handleColorChange()}>{title}</h3>
+                            <p>{description}</p>
+                        </div>
+                        <div className="imageWrapper">
+                            <img src={imgUrl} alt="" />
+                        </div>
+                    </div>
+                ) : (
+                    <></>
+                )
+            }
         </>
     )
 };
