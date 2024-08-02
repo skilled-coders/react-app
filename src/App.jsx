3d0ac81048cc1524f8f95bb20954b2511848796b
Yash Patil <yashpatil@Yashs-MacBook-Pro.local>
2024-08-02 17:36:58 +0530

    conditional rendering; json-server; use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css'
 import Header from './components/Header'
 import BlogList from './components/BlogList';
@@ -7,7 +7,9 @@ function App() {
 	const [user, setUser] = useState("Bharat"); // ES6 - destructuring
 	const [age, setAge] = useState(20);
 
-	const clickHandler = (e, name, age) => {
+	const [counter, setCounter] = useState(0);
+
+	const clickHandler = (event, name, age) => {
 		setUser(name);
 		setAge(age)
 	}
@@ -16,25 +18,62 @@ function App() {
 		{
 			id: 1,
 			title: "My First Blog",
-			details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque distinctio itaque similique? Minus est cum temporibus molestias, amet asperiores facilis. Temporibus quidem blanditiis velit explicabo accusamus iusto et qui consequuntur."
+			details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque distinctio itaque similique? Minus est cum temporibus molestias, amet asperiores facilis. Temporibus quidem blanditiis velit explicabo accusamus iusto et qui consequuntur.",
+			author: "Pratham"
 		},
 		{
 			id: 2,
 			title: "My Second Blog",
-			details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque distinctio itaque similique? Minus est cum temporibus molestias, amet asperiores facilis. Temporibus quidem blanditiis velit explicabo accusamus iusto et qui consequuntur."
+			details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque distinctio itaque similique? Minus est cum temporibus molestias, amet asperiores facilis. Temporibus quidem blanditiis velit explicabo accusamus iusto et qui consequuntur.",
+			author: "Jatin"
 		},
 		{
 			id: 3,
 			title: "My Third Blog",
-			details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque distinctio itaque similique? Minus est cum temporibus molestias, amet asperiores facilis. Temporibus quidem blanditiis velit explicabo accusamus iusto et qui consequuntur."
+			details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque distinctio itaque similique? Minus est cum temporibus molestias, amet asperiores facilis. Temporibus quidem blanditiis velit explicabo accusamus iusto et qui consequuntur.",
+			author: "Bharat"
+		}
+	]);
+
+	const [oldestBlogs, setOldestBlogs] = useState([
+		{
+			id: 1,
+			title: "My First Old Blog",
+			details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque distinctio itaque similique? Minus est cum temporibus molestias, amet asperiores facilis. Temporibus quidem blanditiis velit explicabo accusamus iusto et qui consequuntur.",
+			date: new Date().toDateString()
+		},
+		{
+			id: 2,
+			title: "My Second Old Blog",
+			details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque distinctio itaque similique? Minus est cum temporibus molestias, amet asperiores facilis. Temporibus quidem blanditiis velit explicabo accusamus iusto et qui consequuntur.",
+			date: new Date().toDateString()
+		},
+		{
+			id: 3,
+			title: "My Third Old Blog",
+			details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque distinctio itaque similique? Minus est cum temporibus molestias, amet asperiores facilis. Temporibus quidem blanditiis velit explicabo accusamus iusto et qui consequuntur.",
+			date: new Date().toDateString()
 		}
 	]);
 
 	const secondClickHandler = (e) => {
-		for (let index = 0; index < 10; index++) {
-			console.log(index, e)
-		}
+		setCounter(counter + 1);
 	}
+
+	// Triggers after each re-render
+	useEffect(() => {
+		console.log("Use Effect got triggered");
+	});
+
+	// Triggers only when we refresh the page / component get rendered for the first time
+	useEffect(() => {
+		console.log("Use Effect got triggered");
+	}, []);
+
+	// Triggers whenver the values of the dependency array changes
+	useEffect(() => {
+		console.log("Use Effect got triggered");
+	}, [counter, user]);
 
 	return (
 		<>
@@ -42,10 +81,12 @@ function App() {
 
 			<h5>{user}</h5>
 			<h6>{age}</h6>
+			<h2>{counter}</h2>
 			<button onClick={(e) => clickHandler(e, "Yash", 29)}>Click Me</button>
 			<button onClick={secondClickHandler}>Click Me</button>
 			<div className="body">
-				<BlogList blogs={blogs} name={user} aayu={age} />
+				<BlogList blogs={blogs} title={"Recent Blogs"} />
+				<BlogList blogs={oldestBlogs} title={"Oldest Blogs"} /> {/* Reusability */}
 			</div>
 		</>
 	)
--- src/components/BlogList/index.jsx
+++ src/components/BlogList/index.jsx
@@ -1,8 +1,14 @@
-function BlogList({ blogs }) {
+function BlogList({ blogs, title }) {
     return (
         <div className="blogListContainer">
+            <h2>{title}</h2>
             {
                 blogs.map((blog) => {
+                    // let author = "";
+                    // if (blog.author) {
+                    //     author = `- ${blog.author}`
+                    // }
+                    // let authorname = blog.author ? `- ${blog.author}` : "";
                     return (
                         <div key={blog.id} className="blogWrapper">
                             <div>
@@ -11,6 +17,22 @@ function BlogList({ blogs }) {
                             <div>
                                 <p className='montserrat-300'>{blog.details}</p>
                             </div>
+                            {/* Conditional Rendering */}
+                            {
+                                blog.author ? (
+                                    <div>
+                                        <span>-  {blog.author}</span>
+                                    </div>
+                                ) : <></>
+                            }
+                            {
+                                blog.date ? (
+                                    <div>
+                                        <span>Date: {blog.date}</span>
+                                    </div>
+                                ) : <></>
+                            }
+                            <button onClick={() => alert("Delete " + blog.id)}>Delete</button>
                         </div>
                     )
                 })
